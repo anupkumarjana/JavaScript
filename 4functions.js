ef8795b55cc481f8b4fcf60247277f97dec05651
anupkumarjana <janaanup7872@gmail.com>
2024-02-23 11:38:36 +0530

    functions modified

---
+++ 4functions.js
@@ -0,0 +1,25 @@
+let x = 10;
+a(); // this will be executed first beacuse of hoisting
+b(); // this will be executed scecond
+console.log(x); // this will be executed third
+c(); // this will be last thing to execute
+
+function a() {
+  var x = 100;
+  console.log(x); // x is function scoped here
+}
+
+function b() {
+  var x = 200;
+  console.log(x); // x is function scoped here
+}
+function c() {
+  var x = 300;
+  console.log(x); // x is function scoped here
+}
+
+//Output will be
+// 100
+// 200
+// 10
+// 300
